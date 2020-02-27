302a2072501152b454c5572f91da2069eaf74733
Fabio Fantacuzzi <f.fantacuzzi@gmail.com>
2020-02-27 17:12:55 +0100

    1-rel

---
+++ js/main.js
@@ -0,0 +1,6 @@
+$('.fas.fa-bars').click(function () {
+    $('.hamburger-menu').addClass('active');
+});
+$('.fas.fa-times').click(function () {
+    $('.hamburger-menu').removeClass('active');
+});
